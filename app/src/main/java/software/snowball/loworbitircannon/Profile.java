0cf51db7acda863139dc0ca26e496e625b3e896f
RudyZach <rudyzachary98@gmail.com>
2016-08-08 01:08:55 -0400

    started working on queue

---
+++ app/src/main/java/software/snowball/loworbitircannon/Profile.java
@@ -0,0 +1,14 @@
+package software.snowball.loworbitircannon;
+
+import java.util.ArrayList;
+
+/**
+ * Created by rudyza on 8/8/2016.
+ */
+public class Profile {
+    private ArrayList<String> commands;
+
+    public Profile() {
+        commands = new ArrayList<String>();
+    }
+}
